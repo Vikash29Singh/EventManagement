10c7b6e9472baa6448ed1c5ac6e7751ddbae6079
Vikash Singh <vikashlikes18@gmail.com>
2020-02-26 23:53:29 +0530

    new updates

--- app/src/main/java/com/example/eventmanager/Login.java
+++ app/src/main/java/com/example/eventmanager/Login.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -21,23 +22,27 @@ public class Login extends AppCompatActivity {
     EditText password, useremail;
     Button btn_login;
     // ProgressDialog progressBar;
-    int count=0,count1=3;
+    int count = 0, count1 = 3;
 
     AlertDialog.Builder builder;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+
+    private ProgressDialog mProgress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         firebaseAuth = FirebaseAuth.getInstance();
 
-         useremail = (EditText) findViewById(R.id.useremail);
+        useremail = (EditText) findViewById(R.id.useremail);
         password = (EditText) findViewById(R.id.Password);
 
-        btn_login=(Button)findViewById(R.id.btn_login);
+        btn_login = (Button) findViewById(R.id.btn_login);
 
         // final ProgressBar simpleProgressBar = (ProgressBar) findViewById(R.id.simpleProgressBar);
 
@@ -60,6 +65,17 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 String userEmail = useremail.getText().toString();
                 String userPaswd = password.getText().toString();
+                //_btn_login.setEnabled(false);
+
+
+
+                mProgress = new ProgressDialog(Login.this,
+                        R.style.AppTheme_Dark_Dialog);
+                mProgress.setIndeterminate(true);
+                //mProgress.setTitle("Processing...");
+                mProgress.setMessage("Authenticating");
+                //mProgress.setCancelable(false);
+                mProgress.show();
                 if (userEmail.isEmpty()) {
                     useremail.setError("Provide your Email first!");
                     useremail.requestFocus();
@@ -83,7 +99,7 @@ public class Login extends AppCompatActivity {
                                         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(DialogInterface dialogInterface, int i) {
-                                                startActivity(new Intent(getApplicationContext(),Login.class));
+                                                startActivity(new Intent(getApplicationContext(), Login.class));
                               /* finish();
 
 
@@ -103,9 +119,13 @@ public class Login extends AppCompatActivity {
 
                                 Toast.makeText(getApplicationContext(), "Not sucessfull", Toast.LENGTH_SHORT).show();
                             } else {
+
+
+
                                 FirebaseAuth.getInstance().signOut();
                                 startActivity(new Intent(getApplicationContext(), Dashboard.class));
                                 finish();
+                                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
                             }
                         }
                     });
@@ -123,50 +143,6 @@ public class Login extends AppCompatActivity {
         super.onStart();
         firebaseAuth.addAuthStateListener(authStateListener);
     }
-
-
-/*
-    sign.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-
-        *//*Intent intent = new Intent(getApplicationContext(), Dashboard.class);
-
-
-        intent.putExtra("Name", useremail.getText().toString());
-        startActivity(intent);*//*
-
-     *//*count=count+1;
-        count1=count1-1;*//*
-       // Toast.makeText(Login.this, useremail.getText().toString(), Toast.LENGTH_SHORT).show();
-
-        if (useremail.getText().toString().matches("iot") && password.getText().toString().matches("123")) {
-            Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-
-            Intent intent = new Intent(Login.this, Dashboard.class);
-            //simpleProgressBar.setVisibility(View.VISIBLE);
-
-           intent.putExtra("Name", useremail.getText().toString());
-            startActivity(intent);
-
-            //startActivity(intent);
-        }
-         else {
-
-
-            Toast.makeText(Login.this, "Wrong Credentials", Toast.LENGTH_SHORT).show();
-            //\n Attempt left : "+count1
-
-           *//* if(count>2) {
-                     System.exit(0);
-                    finish();
-                }*//*
-        }
-    }
-});*/
-
-/*    public void registration(View view) {
-    }*/
 }
 
 
