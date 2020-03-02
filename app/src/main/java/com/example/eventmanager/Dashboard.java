794043299176ae2781dd89e47715a8e47675c3b3
ZoRb96 <sauravbhagabati@gmail.com>
2020-03-02 19:04:46 +0530

    sau

--- app/src/main/java/com/example/eventmanager/Dashboard.java
+++ app/src/main/java/com/example/eventmanager/Dashboard.java
@@ -31,8 +31,6 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_dashboard);
 
         //firebase connectivity
